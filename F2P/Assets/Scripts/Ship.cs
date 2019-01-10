6540f6d63318f193008243174128d2fa17c62817
Traqq <alexandre.zandecki@yahoo.com>
2019-01-10 11:27:38 +0100

    bounce boundaries fixed

--- F2P/Assets/Scripts/Ship.cs
+++ F2P/Assets/Scripts/Ship.cs
@@ -174,12 +174,12 @@ public class Ship : MonoBehaviour {
 
     public void Move()
     {
-        if ((directions[id].x > 0 && transform.position.x >= 50))
+        if ((directions[id].x > 0 && transform.position.x >= 48.5))
         {
             directions[id] = new Vector3(-directions[id].x, directions[id].y, directions[id].z);
         }
 
-        if ((directions[id].x < 0 && transform.position.x <= -50))
+        if ((directions[id].x < 0 && transform.position.x <= -48.5))
         {
             directions[id] = new Vector3(-directions[id].x, directions[id].y, directions[id].z);
         }
