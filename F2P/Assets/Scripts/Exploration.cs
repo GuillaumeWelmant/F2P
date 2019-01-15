bc742aa68ecfb100faf1d99e3647a79bf79f3e39
GuillaumeWelmant <guillaume.welmant@gmail.com>
2019-01-15 09:13:02 +0100

    Explo test

--- F2P/Assets/Scripts/Exploration.cs
+++ F2P/Assets/Scripts/Exploration.cs
@@ -10,6 +10,13 @@ public class Exploration : MonoBehaviour {
     public GameObject dailyInvok;
     public GameObject stdInvok;
 
+    public Card[] cardPool;
+
+    private bool rareMin;
+    private bool epicMin;
+
+    public CardDisplay[] loots; 
+
 	// Use this for initialization
 	void Start ()
     {
@@ -42,5 +49,110 @@ public class Exploration : MonoBehaviour {
         invok.SetActive(true);
         dailyInvok.SetActive(false);
         stdInvok.SetActive(false);
+    }
+
+    public void StartExploration(int i)
+    {
+        StartCoroutine(Explore(i));
+    }
+
+    public IEnumerator Explore(int i)
+    {
+        if (i >= 10)
+        {
+            epicMin = true;
+
+        }
+        else
+        {
+            epicMin = false;
+        }
+
+        List<Card> cardsLooted = new List<Card>();
+        Card cardLooted = null;
+
+        for(int j=0; j<i; j++)
+        {
+            rareMin = true;
+
+            for(int k=0; k<3; k++)
+            {
+                if (k != 2)
+                {
+                    int r = Random.Range(0, cardPool.Length);
+                    cardLooted = cardPool[r];
+
+                    if (cardLooted.rarity == Card.Rarity.epic || cardLooted.rarity == Card.Rarity.legendry)
+                    {
+                        epicMin = false;
+                        rareMin = false;
+                    }
+                    else if (cardLooted.rarity == Card.Rarity.rare)
+                    {
+                        rareMin = false;
+                    }
+
+                    cardsLooted.Add(cardLooted);
+                }
+                else
+                {
+                    if(epicMin && j >= 9)
+                    {
+                        while(cardLooted.rarity != Card.Rarity.epic && cardLooted.rarity != Card.Rarity.legendry)
+                        {
+                            int r = Random.Range(0, cardPool.Length);
+                            cardLooted = cardPool[r];
+                        }
+
+                        epicMin = false;
+                        rareMin = false;
+
+                        cardsLooted.Add(cardLooted);
+                    }
+                    else if (rareMin)
+                    {
+                        while (cardLooted.rarity != Card.Rarity.rare)
+                        {
+                            int r = Random.Range(0, cardPool.Length);
+                            cardLooted = cardPool[r];
+                        }
+
+                        rareMin = false;
+
+                        cardsLooted.Add(cardLooted);
+                    }
+                    else
+                    {
+                        int r = Random.Range(0, cardPool.Length);
+                        cardLooted = cardPool[r];
+
+                        if (cardLooted.rarity == Card.Rarity.epic || cardLooted.rarity == Card.Rarity.legendry)
+                        {
+                            epicMin = false;
+                            rareMin = false;
+                        }
+                        else if (cardLooted.rarity == Card.Rarity.rare)
+                        {
+                            rareMin = false;
+                        }
+
+                        cardsLooted.Add(cardLooted);
+                    }
+                            
+                }
+            }
+
+            for(int z=0; z<loots.Length; z++)
+            {
+                loots[z].card = cardsLooted[z];
+                loots[z].DisplayCard();
+            }
+
+            yield return new WaitForSeconds(1.5f);
+
+        }
+
+
+                
     }
 }
